2ed80ca6df40e909b3f8233cadc13c7ec7905fbf
Alex Babichev <alex.babichev@me.com>
2019-03-18 17:01:32 +0100

    auto parse current url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Breadcrumb } from './breadcrumbs/breadcrumbs.component';
 
 @Component({
   selector: 'app-root',
@@ -9,65 +8,16 @@ import { Breadcrumb } from './breadcrumbs/breadcrumbs.component';
 export class AppComponent {
   title = 'breadcrumbs';
 
-  breadcrumbs1: Breadcrumb[] = [{
-    label: 'Meal Planning',
-    link: '/orders'
-  }, {
-    label: 'Service Offering'
-  }, {
-    label: 'Courses'}
+  testBreadcrumbs = [
+    {
+      label: 'Meal Planning',
+      link: '/orders'
+    }, {
+      label: 'Service Offering'
+    }, {
+      label: 'Courses'
+    }
   ];
 
-  breadcrumbs1a: Breadcrumb[] = [{
-    label: 'Meal Planning',
-    link: '/orders'
-  }, {
-    label: 'Service Offering'
-  }, {
-    label: 'Courses',
-    link: 'listview'
-  }, {
-    label: 'Course TwoCourse'
-  }];
-
-  
-
-  breadcrumbs2: Breadcrumb[] = [{
-    label: 'Meal Planning',
-    link: '/orders'
-  }, {
-    label: 'Service Offering'
-  }, {
-    label: 'Service Templates',
-  }];
-
-  breadcrumbs2a: Breadcrumb[] = [{
-    label: 'Meal Planning',
-    link: '/orders'
-  }, {
-    label: 'Service Offering'
-  }, {
-    label: 'Service Templates',
-    link: 'listview'
-  }, {
-    label: 'HugeRatio3'
-  }];
-
-  breadcrumbs3: Breadcrumb[] = [{
-    label: 'Meal Planning',
-    link: '/orders'
-  }, {
-    label: 'Meal Cycles',
-  }];
-
-  breadcrumbs3a: Breadcrumb[] = [{
-    label: 'Meal Planning',
-    link: '/orders'
-  }, {
-    label: 'Meal Cycles',
-    link: 'listview'
-  }, {
-    label: '3 Cycle'
-  }];
-  
+  constructor() { }
 }
--- src/app/breadcrumbs/breadcrumbs.component.ts
+++ src/app/breadcrumbs/breadcrumbs.component.ts
@@ -1,9 +1,20 @@
 import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 export interface Breadcrumb {
   label: string;
   link?: string;
 }
+
+export const routesMock = [
+  '/new/mp/courses',
+  '/new/mp/courses/1677',
+  '/new/mp/service-templates',
+  '/new/mp/service-templates/1650',
+  '/new/mp/meal-cycles',
+  '/new/mp/meal-cycles/1621'
+];
 
 // Use the CheckOnce strategy, meaning that automatic change 
 // detection is deactivated until reactivated by setting the 
@@ -19,9 +30,45 @@ export interface Breadcrumb {
 export class BreadcrumbsComponent {
 
   @Input()
-  breadcrumbs: Breadcrumb[] = [];
+  breadcrumbs?: Breadcrumb[];
 
-  constructor() { }
+  @Input()
+  url?: string;
+
+  @Input()
+  childLabel?: string;
+
+  constructor(private router: Router) {
+    this.url = this.url || this.router.url;
+  }
+
+  ngOnInit() {    
+    if (this.breadcrumbs)
+      return;
+
+    const url = this.url.split('/');
+    const hasChild = !!url[4];
+    const label = url[3]
+      .split('-')
+      .map(word => word.charAt(0).toUpperCase() + word.substring(1))
+      .join(' ');
+
+    const returnArr: Breadcrumb[] = [{
+      label: 'Meal Planning',
+      link: '/orders'
+    }, {
+      label: 'Service Offering'
+    }];
+
+    if (hasChild) {
+      returnArr.push({ label, link: url.slice(0, -1).join('/') });
+      returnArr.push({ label: this.childLabel || 'childLabel' })
+    } else {
+      returnArr.push({ label })
+    }
+
+    this.breadcrumbs = returnArr;
+  }
 
 }
 
